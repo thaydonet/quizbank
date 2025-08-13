a39268733e7ed1330a6b4df96cdf402359945fe3
thaydonet <thaydo.net@gmail.com>
2025-08-13 10:34:21 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,8 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-white border-t border-gray-200">
       <div className="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <p className="text-center text-sm text-gray-500">
-          © {currentYear} Ngân Hàng Câu Hỏi Toán AI. Quiz - Bank - Do - 2025.
+        <p className="text-center text-sm text-blue-500">
+          © {currentYear} Ngân Hàng Câu Hỏi Toán AI. QuizBankDo.
         </p>
       </div>
     </footer>
