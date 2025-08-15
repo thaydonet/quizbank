5fa89fabfd453aa183b405ef77b2bdc629d1911c
thaydonet <thaydo.net@gmail.com>
2025-08-15 14:43:19 +0700

    Updated Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
     <footer className="bg-white border-t border-gray-200">
       <div className="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <p className="text-center text-sm text-gray-500">
-          © {currentYear} Ngân Hàng Câu Hỏi Toán AI. Thầy Đồ - <Link to="/">Home</Link>.
+          © {currentYear} Ngân Hàng Câu Hỏi Toán AI. Thầy Đồ - <Link to="/" className="text-2xl font-bold text-indigo-300">Home</Link>.
         </p>
       </div>
     </footer>
